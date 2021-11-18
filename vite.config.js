cc41663634a725044a4a31e4ae8ed779fc7547c1
paccao <16489683+paccao@users.noreply.github.com>
2021-11-18 09:49:11 +0100

    vitepwa plugin initial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [
+		react(),
+		VitePWA({
+			includeAssets: [
+				"favicon.svg",
+				"favicon.ico",
+				"robots.txt",
+				"apple-touch-icon.png",
+			],
+			manifest: {
+				name: "Instablam",
+				short_name: "Instablam",
+				description: "Take pictures with your phone or computer's webcam and scroll through your gallery of pictures.",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+		}),
+	],
 })
