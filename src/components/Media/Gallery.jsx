7326e90a49bed377f34e8afd87c1da62654676b5
paccao <16489683+paccao@users.noreply.github.com>
2021-11-26 16:07:55 +0100

    fixed styling on gallery

--- src/components/Media/Gallery.jsx
+++ src/components/Media/Gallery.jsx
@@ -20,28 +20,34 @@ export default function Gallery() {
 								width="500"
 								height="500"
 							/>
-							<figcaption aria-label="Photo location and date information">{`${obj.location}, ${obj.takenAt}`}</figcaption>
-							<a
-								aria-label="Download selected photo"
-								href={obj.url}
-								download
-							>
-								<MdDownload />
-								<p>Download</p>
-							</a>
-							<Button
-								aria-label="Delete selected photo"
-								onClick={() =>
-									deletePhoto(
-										galleryPictures,
-										setGalleryPictures,
-										obj.id,
-									)
-								}
-							>
-								<BsTrash />
-								<p>Delete</p>
-							</Button>
+							<Figc aria-label="Photo location and date information">{`${obj.location}, ${obj.takenAt}`}</Figc>
+							<BottomLinks>
+								<a
+									aria-label="Download selected photo"
+									href={obj.url}
+									download
+								>
+									<MiniContainer>
+										<MdDownload />
+										<p>Download</p>
+									</MiniContainer>
+								</a>
+								<Button
+									aria-label="Delete selected photo"
+									onClick={() =>
+										deletePhoto(
+											galleryPictures,
+											setGalleryPictures,
+											obj.id,
+										)
+									}
+								>
+									<MiniContainer>
+										<BsTrash />
+										<p>Delete</p>
+									</MiniContainer>
+								</Button>
+							</BottomLinks>
 						</Card>
 					)
 				})}
@@ -49,11 +55,34 @@ export default function Gallery() {
 	)
 }
 
+const BottomLinks = styled.div`
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+`
+const Figc = styled.figcaption`
+	color: #fff;
+	text-align: center;
+`
+const MiniContainer = styled.div`
+	margin: 0.5em 0em;
+	display: flex;
+	align-items: center;
+`
+
 const Card = styled.div`
 	border-bottom: 1px solid #222;
 	border-bottom-left-radius: 10px;
 	border-bottom-right-radius: 10px;
 	margin-bottom: 0.5rem;
+
+	a {
+		color: #dd88dd;
+	}
+
+	button {
+		color: #f50a1d;
+	}
 `
 
 const Image = styled.img`
